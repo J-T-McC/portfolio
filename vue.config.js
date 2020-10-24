dbccf40ce5a04b8f530c0f2f08cade3663d1dd30
tmccarney <tm@sentisresearch.com>
2020-10-23 19:10:38 -0700

    Fixed og meta img

--- vue.config.js
+++ vue.config.js
@@ -7,27 +7,4 @@ module.exports = {
             title: 'Portfolio: Tyson McCarney',
         },
     },
-    configureWebpack: {
-        module: {
-            rules: [
-                {
-                    test: /me\d*\.png/, // <= modify this to suit your needs
-                    use: [
-                        {
-                            loader: "url-loader",
-                            options: {
-                                limit: 4096,
-                                fallback: {
-                                    loader: "file-loader",
-                                    options: {
-                                        name: "img/[name].[ext]", // <= note how the hash is removed
-                                    },
-                                },
-                            },
-                        },
-                    ],
-                },
-            ],
-        },
-    }
 }
