e5fc00f7a57f704629c779c5899079eb0c5ef875
tmccarney <tm@sentisresearch.com>
2020-10-24 21:19:20 -0700

    Updated images to cloudinary

--- src/components/content/Contact.vue
+++ src/components/content/Contact.vue
@@ -2,7 +2,7 @@
   <div class="relative bg-blue-500 overflow-hidden pt-5 pb-10 md:p-0" id="contact">
     <div class="lg:w-1/2 max-w-screen-xl">
       <div class="relative bg-blue-500 lg:w-full hidden lg:block">
-        <img class="w-full object-cover lg:w-full" src="@/assets/images/contact.jpg" alt="">
+        <img class="w-full object-cover lg:w-full" src="https://res.cloudinary.com/ddaji66m6/image/upload/v1603599254/portfolio/contact_vyld16.jpg" alt="">
       </div>
       <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <div class="flex flex-col h-full w-full items-center justify-center py-6 px-6 bg-blue-500">
