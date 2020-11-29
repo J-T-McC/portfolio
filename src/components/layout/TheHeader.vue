5377ad85b112d07e3dc619a2f87d4e5e061a6bb0
J-T-McC <tyson_mcc@hotmail.com>
2020-11-28 22:23:13 -0800

    Added linkedin and github

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -14,7 +14,6 @@
            border-blue-500
            border-solid
            z-50">
-
       <div class="flex items-center flex-shrink-0 mr-6">
         <svg-logo></svg-logo>
         <span class="font-semibold text-xl tracking-tight cursor-pointer" v-scroll-to="'#me'">Tyson McCarney</span>
@@ -43,12 +42,14 @@
             duration-500
             ease-in-out
             w-full
+            justify-between
             flex-grow
             lg:flex
             sm:items-center
             md:items-center
             lg:w-auto"
           :class="{'hidden': !open, 'block': open }">
+
         <div class="text-gray-700 text-md font-light tlg:flex-grow">
           <a v-scroll-to="'#about'"
              href="#about"
@@ -59,6 +60,15 @@
              href="#contact"
              class="block mt-4 lg:inline-block lg:mt-0 hover:text-blue-400 mr-4">
             Contact
+          </a>
+        </div>
+
+        <div class="flex flex-row sm:mt-4 lg:mt-0">
+          <a href="https://www.linkedin.com/in/tyson-mccarney-7582ba71/" class="mr-3" target="_blank">
+            <img alt="LinkedIn" class="w-5 inline" src="@/assets/images/linkedin-logo.svg">
+          </a>
+          <a href="https://github.com/J-T-McC" target="_blank">
+            <img alt="GitHub" class="w-5 inline" src="@/assets/images/github-logo.svg">
           </a>
         </div>
       </div>
